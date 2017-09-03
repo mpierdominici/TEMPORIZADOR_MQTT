65f031c37bd085e619cfb4f2e9276c3e49f83a4e
Matias <mpierdominici@itba.edu.ar>
2017-09-03 13:18:55 -0300

    archivo vase, se provo la suscripcion a topicos y toda la inicializacion vase

---
+++ timer_mqtt.ino
@@ -0,0 +1,121 @@
+//***********************************//
+//Modulo temporizador para pileta    //
+//                                   //
+//Matias Pierdominici                //
+//mpierdominici@itba.edu.ar          //
+//***********************************//
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#define DEBUGG
+char * ssid ="WIFI Pier";
+char * pass ="pagle736pagle";
+unsigned int mqttPort=1883;
+
+const char MqttUser[]="timerbox";
+const char MqttPassword[]="1234";
+const char MqttClientID[]="box";
+
+IPAddress mqttServer(192,168,0,116);
+
+WiFiClient wclient;
+PubSubClient mqtt_client(wclient);
+
+void callback(char* topic, byte* payload, unsigned int length);
+void  debug_message (char * string, bool newLine)
+{
+#ifdef DEBUGG
+  if(string !=NULL)
+  {
+    if (!newLine)
+    {
+      Serial.print(string);
+    }else
+    {
+      Serial.println(string);
+    }
+  }
+  #endif
+}
+
+void setUpWifi(char * ssid, char * pass)
+{
+  String ip;
+  debug_message(" ",true);
+  debug_message(" ",true);
+  debug_message("Conectandose a: ",false);
+  debug_message(ssid,true);
+
+  WiFi.begin(ssid,pass);
+
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    debug_message(".",false);
+  }
+  debug_message(" ",true);
+  debug_message("Coneccion realizada",true);
+  debug_message("La ip es: ",false);
+  ip=WiFi.localIP().toString();
+  debug_message((char *)ip.c_str(),true);
+}
+
+void setUpMqtt(void)
+{
+  mqtt_client.setServer(mqttServer,mqttPort);
+  mqtt_client.setCallback(callback);
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length)
+{
+  payload[length]='\n';
+  debug_message("Llego un mensage, topic:",false);
+  debug_message(topic,false);
+  debug_message(", payload : ",false);
+  debug_message((char *)payload,true);
+}
+
+void reconnect()
+{
+  while(!mqtt_client.connected())
+  {
+    debug_message("Intentando conectar al servidor MQTT",true);
+    if (mqtt_client.connect(MqttClientID,MqttUser,MqttPassword))
+      {
+            debug_message("conectado",true);
+  
+  
+            // ...suscrivirse a topicos
+            mqtt_client.subscribe("timer/setTime");
+            mqtt_client.subscribe("timer/start");
+            mqtt_client.subscribe("timer/stop");
+            
+
+
+      }
+      else
+      {
+        debug_message("intentando conetarse al broker",true);
+        delay(3000);
+      }
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  setUpWifi(ssid,pass);
+  setUpMqtt();
+
+}
+
+
+void loop() {
+  if (!mqtt_client.connected()) 
+  {
+      reconnect();
+      
+ }
+ mqtt_client.loop(); 
+ 
+
+}
