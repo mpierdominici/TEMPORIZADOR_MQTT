d7537e9bb29bb27e65a921908ec27ce1b362c652
Matias <mpierdominici@itba.edu.ar>
2017-09-03 22:23:44 -0300

    ya se agrego la parte de timer, falta comentar y probar otra gpio

--- timer_mqtt.ino
+++ timer_mqtt.ino
@@ -7,6 +7,47 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #define DEBUGG
+
+#define WATER_BOMB_PIN BUILTIN_LED 
+#define WATCH_DOG 13
+#define SEC_TO_MILISEC(x) ((x)*1000) 
+#define STOP_BOMB 1
+#define RUN_BOMB 2
+#define NOTHING 3
+
+
+
+
+class myTimer
+{
+  public:
+  myTimer(unsigned int seconds=0);
+  bool timeOver(void);
+  void setNewTime(unsigned long seconds_);
+  void showInfo();
+  
+  unsigned long seconds;
+  unsigned long startTime;
+  void resetTimer(void);
+    
+};
+
+
+
+class waterBomb
+{
+  public:
+  waterBomb(unsigned int pin_);
+  void onn(){digitalWrite(pin,HIGH);};
+  void off(){digitalWrite(pin,LOW);};
+  private:
+  unsigned int pin;
+  
+};
+
+
+
+
 char * ssid ="WIFI Pier";
 char * pass ="pagle736pagle";
 unsigned int mqttPort=1883;
@@ -19,6 +60,14 @@ IPAddress mqttServer(192,168,0,116);
 
 WiFiClient wclient;
 PubSubClient mqtt_client(wclient);
+
+
+int mode=STOP_BOMB;
+myTimer timer(1);
+waterBomb bomb(WATER_BOMB_PIN);
+
+
+
 
 void callback(char* topic, byte* payload, unsigned int length);
 void  debug_message (char * string, bool newLine)
@@ -68,11 +117,31 @@ void setUpMqtt(void)
 
 void callback(char* topic, byte* payload, unsigned int length)
 {
+  int tiempo=0;
   payload[length]='\n';
+  String message((char *)payload);
   debug_message("Llego un mensage, topic:",false);
   debug_message(topic,false);
   debug_message(", payload : ",false);
   debug_message((char *)payload,true);
+
+  if(!strcmp(topic,"timer/stop"))
+  {
+    mode=STOP_BOMB;
+    debug_message("LLEGO MENSAJE PARA DETENER LA BOMBA",true);
+  }
+  else if (!strcmp(topic,"timer/setTime"))
+  {
+    tiempo=message.toInt();
+    debug_message("llego el tiempo para setear el timer",true);
+    Serial.println(tiempo);
+    timer.setNewTime(tiempo*60);
+  }
+  else if (!strcmp(topic,"timer/start"))
+  {
+     mode=RUN_BOMB;
+     debug_message("llego mensaje para encender la bomba",true);
+  }
 }
 
 void reconnect()
@@ -109,6 +178,7 @@ void setup() {
 }
 
 
+
 void loop() {
   if (!mqtt_client.connected()) 
   {
@@ -116,6 +186,107 @@ void loop() {
       
  }
  mqtt_client.loop(); 
+
+//****************ETAPA TIMER*******************
+
+switch (mode)
+  {
+    case STOP_BOMB:
+    debug_message("se apago el motor",true);
+    bomb.off();
+    mode=NOTHING;
+    break;
+    case RUN_BOMB:
+    if(!timer.timeOver())
+    {
+      debug_message("se prendio el motor",true);
+      bomb.onn();
+      mode=NOTHING;
+    }
+    else
+    {
+     // Serial.println("TimeOut");
+      mode =STOP_BOMB;
+    }
+    break;
+    case NOTHING:
+    break;
+  }
+
  
 
 }
+
+
+
+
+
+//***********************TIMER**********************************
+
+
+
+myTimer::myTimer(unsigned int seconds)
+{
+  setNewTime(seconds);
+}
+
+//timeOver
+//devuelve true si ya paso el tiempo seteado,
+//caso contrario devuelve false
+//
+bool myTimer::timeOver(void)
+{
+  if((millis())>startTime)
+  {
+    resetTimer();
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+void myTimer::resetTimer(void)
+{
+  unsigned long temp=seconds+millis();
+ 
+  startTime=temp;
+  //Serial.print("se llamo a rest timer con: ");
+  //Serial.println(startTime);
+}
+
+void  myTimer::setNewTime(unsigned long seconds_)
+{
+  unsigned long temp=1000*seconds_;
+  //Serial.println(temp);
+  seconds=temp;
+ 
+  //Serial.print("s seteo un timer cada: ");
+  //Serial.print(seconds_);
+  //Serial.print(" se registro un tirmpo de: ");
+  //Serial.println(seconds/1000);
+  resetTimer();
+
+}
+
+void myTimer::showInfo()
+{
+  //Serial.println(startTime);
+  unsigned long dif=startTime-millis();
+  //Serial.print("Remaining time (seconds):");
+  //Serial.println(dif/1000);
+  //Serial.println(startTime);
+  //Serial.println(millis());
+  //Serial.println(seconds/1000);
+}
+
+//*********************BOMBA DE AGUA********************
+
+waterBomb::waterBomb(unsigned int pin_)
+{
+  pin=pin_;
+  pinMode(pin,OUTPUT);
+  off();
+}
+
